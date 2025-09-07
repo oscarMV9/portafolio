3afc7d39673e6af9d8edd9c85bad5e55095800bd
oscar <oscarmontoya119@gmail.com>
2025-09-06 21:42:59 -0500

    dashboard terminado (estilizacion)

--- src/componentesReutilizables/navegacion.jsx
+++ src/componentesReutilizables/navegacion.jsx
@@ -4,12 +4,6 @@ const colores = {
     azulOscuro: "#11243D",
     azulMedio: "#2245B5",
     azulClaro: "#72ABF2",
-    amarilloClaro: "#fef3c7",
-    amarilloMedio: "#fbbf24",
-    amarilloOscuro: "#b45309",
-    grisClaro: "#d1d5db",
-    grisMedio: "#6b7280",
-    grisOscuro: "#374151",
 }
 
 const otrosColores = {
@@ -30,20 +24,20 @@ const Navegacion = () => {
     const colorBarra = colorFondo === colores.azulOscuro ? otrosColores.blanco : otrosColores.gris;
 
     return(
-        <nav className="w-full h-20 border-b-2 border-b-amber-50 flex justify-between items-center p-8 text-amber-50 fixed" style={{backgroundColor: colorFondo}}>
+        <nav className="w-full h-24 border-b-2 border-b-amber-50 flex justify-between items-center p-8 text-amber-50 fixed" style={{backgroundColor: colorFondo}}>
             <ul className="w-full h-0.5 px-4 flex justify-around items-center" style={{backgroundColor: colorBarra}}>
                 <div className="flex justify-around w-3/5">
                     <div className="px-4" style={{backgroundColor: colorFondo}}>
-                        <button className="bg-blue-900 p-1.5 border border-slate-300 rounded hover:bg-amber-800">¿Quien soy?</button>
+                        <button className="bg-blue-900 py-2.5 px-6 border font-serif border-slate-300 rounded-lg hover:bg-blue-600 transition">¿Quien soy?</button>
                     </div>
                     <div className="px-4" style={{backgroundColor: colorFondo}}>                 
-                        <button className="bg-blue-900 p-1.5 border border-slate-300 rounded hover:bg-amber-800">Mis proyectos</button>
+                        <button className="bg-blue-900 py-2.5 px-6 border border-slate-300 rounded-lg hover:bg-blue-600 transition">Mis proyectos</button>
                     </div>
                     <div className="px-4" style={{backgroundColor: colorFondo}}>
-                        <button className="bg-blue-900 p-1.5 border border-slate-300 rounded hover:bg-amber-800">Mis estudios</button>
+                        <button className="bg-blue-900 py-2.5 px-6 border border-slate-300 rounded-lg hover:bg-blue-600 transition">Mis estudios</button>
                     </div>
                     <div className="px-4" style={{backgroundColor: colorFondo}}>
-                        <button className="bg-blue-900 p-1.5 border border-slate-300 rounded hover:bg-amber-800">Contactame!</button>
+                        <button className="bg-blue-900 py-2.5 px-6 border border-slate-300 rounded-lg hover:bg-blue-600 transition">Contactame!</button>
                     </div>
                 </div>
             </ul>
